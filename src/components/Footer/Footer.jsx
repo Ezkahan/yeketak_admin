4c6a79ab0fd76144158ab9e8248243847e1e11fd
ezkahan <ezkahan.dev@gmail.com>
2022-02-04 11:05:12 +0500

    Footer component updated

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,6 +1,6 @@
 const Footer = ({ children }) => {
   return (
-    <footer className="col-span-12 xl:fixed bottom-0 left-0 w-full xl:bg-slate-900 bg-opacity-25 backdrop-blur py-2 rounded-t-2xl">
+    <footer className="col-span-12 xl:fixed bottom-0 left-0 w-full xl:bg-slate-900 xl:bg-opacity-25 backdrop-blur py-2 rounded-t-2xl">
       {children}
     </footer>
   );
